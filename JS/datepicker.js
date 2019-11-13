8118f508c3086063f9e38253acb6edc54ffa8fb6
Victor Martins <victormartinstinoco@live.com>
2019-11-13 03:22:00 -0300

    datepicker alteration to all month default filter on wallet

--- JS/datepicker.js
+++ JS/datepicker.js
@@ -11,4 +11,15 @@ $(function () {
 	
 	if ($('.tomorrow') !== undefined)
 		$('.tomorrow').val((new Date().getDate() + 1) + "/" + (new Date().getMonth() + 1) + "/" + new Date().getFullYear())
+
+	if($('.thisMonthFirstDay') !== undefined)
+		$('.thisMonthFirstDay').val(1 + "/" + (new Date().getMonth() + 1) + "/" + new Date().getFullYear())
+
+	if($('.thisMonthLastDay') !== undefined){
+		var today = new Date();
+		var month = today.getMonth();
+		var year = today.getFullYear();
+		var lastDate = new Date(year, month + 1, 0)
+		$('.thisMonthLastDay').val(lastDate.getDate() + "/" + (new Date().getMonth() + 1) + "/" + new Date().getFullYear())
+	}
 });
