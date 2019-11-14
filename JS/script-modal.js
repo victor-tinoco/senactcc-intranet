404af96b607465953f86c4f8ee4932334931722c
Victor Martins <victormartinstinoco@live.com>
2019-11-13 22:35:06 -0300

    update quant equip logic

--- JS/script-modal.js
+++ JS/script-modal.js
@@ -19,52 +19,71 @@ $('#modalequip').on('show.bs.modal', function (event) {
 	}, function (equip) {
 		window.alert("Ocorreu um erro.")
 	})
+
+	// Limpar campos do último modal aberto
+	ClearCheckbox();
+	ClearSelect();
+	$('.today').val(new Date().getDate() + "/" + (new Date().getMonth() + 1) + "/" + new Date().getFullYear());
 })
 
 $('input[type="checkbox"]').change(function () {
 	const api = ApiAgendamento();
 
-	var dia = $('.datepicker-modal').val();
+	var diaInput = $('.datepicker-modal').datepicker('getDate');
+	var dia = diaInput.getFullYear() + '-' + (diaInput.getMonth() + 1) + '-' + diaInput.getDate();
 
 	var cbManha = $('#check-manha').is(':checked');
 	var cbTarde = $('#check-tarde').is(':checked');
 	var cbNoite = $('#check-noite').is(':checked');
 	
 	if (cbManha)
-		horaRetirada = '07:00';
+		horaRetirada = '07:00:00';
 	else
 		if (cbTarde)
-			horaRetirada = '13:00';
+			horaRetirada = '12:00:00';
 		else
-			horaRetirada = (cbNoite) ? '19:00' : null;
+			horaRetirada = (cbNoite) ? '19:00:00' : null;
 	if (cbNoite)
-		horaDevolucao = '22:30'
+		horaDevolucao = '22:30:00'
 	else
 		if (cbTarde)
-			horaDevolucao = '18:00'
+			horaDevolucao = '18:00:00'
 		else
-			horaDevolucao = (cbManha) ? '12:00' : null;
+			horaDevolucao = (cbManha) ? '12:00:00' : null;
 
 	// Para não quebrar a hora de entrega temos essa regra de negócio, onde o usuario não pode não pode 
 	// alugar num mesmo agendamento somente os horários manhã e noite.
 	if (cbManha && cbNoite && !cbTarde) {
 		window.alert('Para esses horários faça um agendamento de cada vez*')
-		$('#check-manha').prop('checked', false);
-		$('#check-tarde').prop('checked', false);
-		$('#check-noite').prop('checked', false);
+		ClearCheckbox();
+		ClearSelect();
 	}
-
 	var id = $('#idEquip').val()
 
-	console.log(dia, horaRetirada, horaDevolucao, id)
+	api.ConsultarDisponibilidade(dia, horaRetirada, horaDevolucao, id, function(data) {
+		const selectQuant = $('.select-quant');
+		selectQuant.empty();
+		quantidadeDisponivel = data;
+		var html = '';
+		for (let i = 0; i <= quantidadeDisponivel; i++) {
+			var text = '<option>' + i + '</option>';
+			html += text;
+		}
+		selectQuant.html(html);
+	}, function(data) {
 
-	// Falta ver como será o formato a ser enviado a data e as horas, não aceita no formato padrão.
-	// api.ConsultarDisponibilidade(dia, horaRetirada, horaDevolucao, id, function(data) {
+	}, function(data) {
 
-	// }, function(data) {
-
-	// }, function(data) {
-
-	// },function(){ window.alert('Ocorreu um erro.') })
+	},function(){ console.log('Foi inserido uma data ou períodos nulos ou indefinidos.') })
 });
 
+function ClearCheckbox(){
+	$('#check-manha').prop('checked', false);
+	$('#check-tarde').prop('checked', false);
+	$('#check-noite').prop('checked', false);
+}
+
+function ClearSelect(){
+	var text = '<option selected>0</option>';
+	$('.select-quant').html(text)
+}
