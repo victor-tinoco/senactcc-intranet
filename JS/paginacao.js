979da91dc9b591990aa6796ebb03c66d66cd5b54
Victor Martins <victormartinstinoco@live.com>
2019-11-21 22:33:12 -0300

    Começo da estruturação da paginação

--- JS/paginacao.js
+++ JS/paginacao.js
@@ -1,28 +1,27 @@
 $(function(){
-
-    var api = ApiEquipamento();
-    var equipQuantity;
-    var quantPages;
+    const api = ApiEquipamento();
+    let equipQuantity;
+    let quantPages;
     api.Paginacao(function(dados){
-        equipQuantity = dados;
+        equipQuantity = (dados != 0) ? dados : 1;
         quantPages = Math.ceil(equipQuantity / 12);
 
-
-        console.log(quantPages, equipQuantity);
     }, function(dados){
         window.alert('Ocorreu um erro.')
     })
 
-    $('.pagination-js').html(
-        '<div class="col d-flex justify-content-center">' +
-            '<ul class="p-0 m-0 mt-2 d-flex" style="list-style-type: none;">' +
-                '<li class="page-links back only-disabled"><</li>' +
-                '<li class="page-links main only">1</li>' +
-                '<li class="page-links next ">2</li>' +
-                '<li class="page-links next-2">3</li>' +
-                '<li class="page-links next">></li>' +
-            '</ul>' +
-        '</div>'
-    )
+    // Criar uma função que recebe o total de pages e qual foi clicada, que retorna um objeto que contem o back, main, second, third, next e preencher
+    
+    const containerPagination = '<div class="col d-flex justify-content-center">' +
+                                    '<ul class="p-0 m-0 mt-2 d-flex" style="list-style-type: none;">' +
+                                        '<li class="page-links back"><</li>' +
+                                        '<li class="page-links main only">1</li>' +
+                                        '<li class="page-links second ">2</li>' +
+                                        '<li class="page-links third">3</li>' +
+                                        '<li class="page-links next">></li>' +
+                                    '</ul>' +
+                                '</div>';
+
+    $('.pagination-js').html(containerPagination);
 })
 
