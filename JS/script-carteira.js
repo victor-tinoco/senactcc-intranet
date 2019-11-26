7f3fddf465f00f3623f07a253be5aabbcf2c4659
Victor Martins <victormartinstinoco@live.com>
2019-11-25 22:35:13 -0300

    Wallet estática

--- JS/script-carteira.js
+++ JS/script-carteira.js
@@ -1,2 +1,23 @@
+$(function(){
+    expandCardEvent();
+})
 
+function expandCardEvent() {
+	const headerCard = $('.header-card');
 
+	headerCard.click(function (e) {
+		const bodyCard = $(this).parent().find('.body-card');
+		const expandIcon = $(this).find('.expand');
+
+		if (bodyCard.hasClass('d-none')) {
+			bodyCard.removeClass('d-none');
+			expandIcon.removeClass('fa-chevron-down');
+			expandIcon.addClass('fa-chevron-up');
+		}
+		else {
+			bodyCard.addClass('d-none');
+			expandIcon.removeClass('fa-chevron-up');
+			expandIcon.addClass('fa-chevron-down');
+		}
+	})
+}
