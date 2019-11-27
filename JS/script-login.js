f5982d59d9671c745941bad68b66c8d377476fea
gabriel yago <gabriellok1212@gmail.com>
2019-11-27 17:14:04 -0300

    loginmobile arrumada

--- JS/script-login.js
+++ JS/script-login.js
@@ -32,3 +32,30 @@ $('#formLogin').submit(function (form)
         $('#Senha').val('') 
     })
 })
+$('#formLogin2').submit(function (form)
+{
+    form.preventDefault();
+    var dados = {
+        usuario: $('#Login').val(),
+        senha: $("#Senha").val()
+    }
+
+    api.FazerLogin(dados, function(dados){
+        Cookies.set('site_autenticado', dados.Token, {expires: 1})
+        window.location.href = urlSite + 'equipamentos.html';
+    }, function(dados){
+        $('.confirm-button').attr('disabled','disabled');
+        $('.confirm-button').val('Carregando...');
+    }, function(dados){
+        $('.confirm-button').removeAttr('disabled');
+        $('.confirm-button').val('Próximo'); 
+    }, function(dados){
+        if (dados.status == 401) {
+            window.alert("Usuario e/ou senha invalidos");
+        }
+        else {
+            window.alert("Ocorreu um erro ao efutuar o login");
+        }
+        $('#Senha').val('') 
+    })
+})
